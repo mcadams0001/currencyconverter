fb61ed8cac350ea690b2871ab166ec9ef2b39e19
Adam Sojka <mcadams2001-as@yahoo.co.uk>
2017-01-27 22:39:16 +0000

    Fix issue with handlebars call.

--- web/src/main/webapp/js/common.js
+++ web/src/main/webapp/js/common.js
@@ -1,65 +1,61 @@
-var common = new Common();
+var common = {};
 
-function Common() {
-    this.isValidDouble = function(value) {
-        if(value === '' || value === undefined) return false;
-        return value.match(/^-?\d*(\.\d+)?$/);
-    };
+common.isValidDouble = function(value) {
+    if(value === '' || value === undefined) return false;
+    return value.match(/^-?\d*(\.\d+)?$/);
+};
 
-    this.isValidDate = function (value) {
-        if (value == null || value === '') return true;
-        return value.toLowerCase().match(/^(\d{1,2})(\/|-)(?:(\d{1,2})|(jan)|(feb)|(mar)|(apr)|(may)|(jun)|(jul)|(aug)|(sep)|(oct)|(nov)|(dec))(\/|-)(\d{4})$/i);
-    };
+common.isValidDate = function (value) {
+    if (value == null || value === '') return true;
+    return value.toLowerCase().match(/^(\d{1,2})(\/|-)(?:(\d{1,2})|(jan)|(feb)|(mar)|(apr)|(may)|(jun)|(jul)|(aug)|(sep)|(oct)|(nov)|(dec))(\/|-)(\d{4})$/i);
+};
 
-    this.loadTemplate = function (templateName, context, element) {
-        var htmlContents = Handlebars.templates[templateName](context, null);
-        $('#' + element).html(htmlContents);
-    };
+common.loadTemplate = function (templateName, context, element) {
+    var htmlContents = Handlebars.templates[templateName](context);
+    $('#' + element).html(htmlContents);
+};
 
-    this.successHandler = function(url, textStatus, jqXHR, handleAfterInitialization, handlePreInitialization, element) {
-        var context = JSON.parse(jqXHR.responseText);
-        if (handlePreInitialization != null) {
-            handlePreInitialization(context);
-        }
-        var html = Handlebars.templates[context.viewName](context, null);
-        $('#' + element).html(html);
-        if (handleAfterInitialization != null) {
-            handleAfterInitialization(context);
-        }
-    };
+common.successHandler = function(url, textStatus, jqXHR, handleAfterInitialization, handlePreInitialization, element) {
+    var context = JSON.parse(jqXHR.responseText);
+    if (handlePreInitialization != null) {
+        handlePreInitialization(context);
+    }
+    common.loadTemplate(context.viewName, context, element);
+    if (handleAfterInitialization != null) {
+        handleAfterInitialization(context);
+    }
+};
 
-    this.errorHandler = function(xhr, element) {
-        var msg = "The application has encountered an error. If this problem persists please contact the Administrator";
-        $('#' + element).html(msg + xhr.statusText);
-        return false;
-    };
+common.errorHandler = function(xhr, element) {
+    var msg = "The application has encountered an error. If this problem persists please contact the Administrator";
+    $('#' + element).html(msg + xhr.statusText);
+    return false;
+};
 
-    this.loadHandleBarPage = function (url, element, handleAfterInitialization, handlePreInitialization) {
-        jQuery.support.cors = true;
-        $.ajax(
-            {
-                cache: false,
-                url: url,
-                type: 'GET',
-                dataType: 'json',
-                error: function (xhr) {
-                    common.errorHandler(xhr, element);
-                },
-                success: function (response, textStatus, jqXHR) {
-                    common.successHandler(response, textStatus, jqXHR, handleAfterInitialization, handlePreInitialization, element);
-                }
-            });
-
-    };
-
-    this.dateFieldPicker = function (elementId) {
-        $("#" + elementId).datepicker({
-            autoSize: true,
-            dateFormat: 'd-M-yy',
-            showOn: "button",
-            buttonImage: "/images/calendar.gif",
-            buttonImageOnly: true
+common.loadHandleBarPage = function (url, element, handleAfterInitialization, handlePreInitialization) {
+    jQuery.support.cors = true;
+    $.ajax(
+        {
+            cache: false,
+            url: url,
+            type: 'GET',
+            dataType: 'json',
+            error: function (xhr) {
+                common.errorHandler(xhr, element);
+            },
+            success: function (response, textStatus, jqXHR) {
+                common.successHandler(response, textStatus, jqXHR, handleAfterInitialization, handlePreInitialization, element);
+            }
         });
-    };
 
-}
+};
+
+common.dateFieldPicker = function (elementId) {
+    $("#" + elementId).datepicker({
+        autoSize: true,
+        dateFormat: 'd-M-yy',
+        showOn: "button",
+        buttonImage: "/images/calendar.gif",
+        buttonImageOnly: true
+    });
+};
--- web/src/main/webapp/js/currency.js
+++ web/src/main/webapp/js/currency.js
@@ -1,93 +1,89 @@
-var currency = new Currency();
+var currency = {};
 
-function Currency() {
+currency.initialize = function() {
+    currency.loadForm();
+    currency.loadHistory();
+};
 
-    this.initialize = function() {
-        currency.loadForm();
-        currency.loadHistory();
-    };
+currency.validateInputs = function() {
+    var status = true;
+    var currencyFrom = $('#from').val();
+    var currencyTo = $('#to').val();
+    var asOfDate = $('#asOfdate').val();
+    var errorMsg = '';
+    if(currencyFrom === currencyTo) {
+        status = false;
+        errorMsg = 'Please select different from and to currencies.<br/>';
+    }
+    if(!common.isValidDouble($('#amount').val())) {
+        status = false;
+        errorMsg += 'Please provide a numeric amount.<br/>';
+    }
+    if(asOfDate != '' && !common.isValidDate(asOfDate)) {
+        status = false;
+        errorMsg += 'Please provide date in valid format dd-MMM-yyyy';
+    }
+    if(status === false) {
+        $('#error').html(errorMsg);
+    }
+    return status;
+};
 
-    this.validateInputs = function() {
-        var status = true;
-        var currencyFrom = $('#from').val();
-        var currencyTo = $('#to').val();
-        var asOfDate = $('#asOfdate').val();
-        var errorMsg = '';
-        if(currencyFrom === currencyTo) {
-            status = false;
-            errorMsg = 'Please select different from and to currencies.<br/>';
-        }
-        if(!common.isValidDouble($('#amount').val())) {
-            status = false;
-            errorMsg += 'Please provide a numeric amount.<br/>';
-        }
-        if(asOfDate != '' && !common.isValidDate(asOfDate)) {
-            status = false;
-            errorMsg += 'Please provide date in valid format dd-MMM-yyyy';
-        }
-        if(status === false) {
-            $('#error').html(errorMsg);
-        }
-        return status;
-    };
+currency.loadForm = function() {
+    common.loadHandleBarPage("currency.html", "formContainer", currency.handleAfterInitialization, null);
+};
 
-    this.loadForm = function() {
-        common.loadHandleBarPage("currency.html", "formContainer", currency.handleAfterInitialization, null);
-    };
+currency.loadHistory = function() {
+    common.loadHandleBarPage("history.html", "historyContainer", null, null);
+};
 
-    this.loadHistory = function() {
-        common.loadHandleBarPage("history.html", "historyContainer", null, null);
-    };
+currency.handleAfterInitialization = function(context) {
+    currency.setupAjaxForm();
+    common.dateFieldPicker('asOfdate');
+};
 
-    this.handleAfterInitialization = function(context) {
-        currency.setupAjaxForm();
-        common.dateFieldPicker('asOfdate');
-    };
+currency.showProgress = function(enable) {
+    if(enable) {
+        $('#resultProgress').show();
+        $('#submitContainer').hide();
+    } else {
+        $('#resultProgress').hide();
+        $('#submitContainer').show();
+    }
+};
 
-    this.showProgress = function(enable) {
-        if(enable) {
-            $('#resultProgress').show();
-            $('#submitContainer').hide();
-        } else {
-            $('#resultProgress').hide();
-            $('#submitContainer').show();
-        }
-    };
-
-    this.handleBeforeSubmit = function() {
-        $('#error').html("");
-        if(!currency.validateInputs()) {
-            return false;
-        }
-        currency.showProgress(true);
-        return true;
-    };
-
-    this.handleError = function(xhr, status, error, $form) {
-        currency.showProgress(false);
+currency.handleBeforeSubmit = function() {
+    $('#error').html("");
+    if(!currency.validateInputs()) {
         return false;
-    };
+    }
+    currency.showProgress(true);
+    return true;
+};
 
-    this.handleSuccess = function (response, textStatus, jqXHR) {
-        currency.showProgress(false);
-        var context = JSON.parse(jqXHR.responseText);
-        if(context.success === "true") {
-            common.loadTemplate("currencyResult", context, "resultContainer");
-            currency.loadHistory();
-        } else {
-            $('#error').html(context.error);
-        }
-        return true;
-    };
+currency.handleError = function(xhr, status, error, $form) {
+    currency.showProgress(false);
+    return false;
+};
 
-    this.setupAjaxForm = function () {
-        jQuery.support.cors = true;
-        $('#convertCurrencyForm').ajaxForm({
-            dataType: 'json',
-            beforeSubmit: currency.handleBeforeSubmit,
-            error: currency.handleError,
-            success: currency.handleSuccess
-        });
-    };
+currency.handleSuccess = function (response, textStatus, jqXHR) {
+    currency.showProgress(false);
+    var context = JSON.parse(jqXHR.responseText);
+    if(context.success === "true") {
+        common.loadTemplate("currencyResult", context, "resultContainer");
+        currency.loadHistory();
+    } else {
+        $('#error').html(context.error);
+    }
+    return true;
+};
 
-}
+currency.setupAjaxForm = function () {
+    jQuery.support.cors = true;
+    $('#convertCurrencyForm').ajaxForm({
+        dataType: 'json',
+        beforeSubmit: currency.handleBeforeSubmit,
+        error: currency.handleError,
+        success: currency.handleSuccess
+    });
+};
