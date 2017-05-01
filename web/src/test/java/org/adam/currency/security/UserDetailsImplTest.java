4d9a889e66fb973400de282ef2de7ed407288beb
Adam <mcadams2001-mail@yahoo.co.uk>
2017-05-01 11:11:23 +0100

    Replaced deprecated Hibernate create criteria with new Hibernate 5.2 API.

--- web/src/test/java/org/adam/currency/security/UserDetailsImplTest.java
+++ web/src/test/java/org/adam/currency/security/UserDetailsImplTest.java
@@ -1,6 +1,8 @@
 package org.adam.currency.security;
 
+import org.adam.currency.builder.RoleBuilder;
 import org.adam.currency.builder.UserBuilder;
+import org.adam.currency.common.RoleNameEnum;
 import org.adam.currency.domain.Role;
 import org.adam.currency.domain.User;
 import org.adam.currency.fixture.AddressFixture;
@@ -10,7 +12,9 @@ import org.junit.Test;
 import org.springframework.security.core.GrantedAuthority;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
@@ -25,16 +29,18 @@ public class UserDetailsImplTest {
 
     @Test
     public void testGetAuthorities() throws Exception {
-        User user = new UserBuilder().withName("test_user").withPassword("1234567890").withFirstName("Test").withLastName("User").withEmailAddress("test_user@domain.com").withBirthDate(LocalDate.of(1981, 4, 1)).withAddress(AddressFixture.TEST_ADDRESS).withRoles(RoleFixture.ROLES).build();
-        user.getRoles().add(new Role(3L, null, null));
-        UserDetailsImpl userDetails = new UserDetailsImpl(USER);
-
+        List<Role> roles = new ArrayList<>();
+        Role standardRole = new RoleBuilder().withId(1L).withName(RoleNameEnum.ROLE_USER).withDescription("Standard User").build();
+        roles.add(standardRole);
+        roles.add(new Role(3L, null, null));
+        User user = new UserBuilder().withName("test_user").withPassword("1234567890").withFirstName("Test").withLastName("User").withEmailAddress("test_user@domain.com").withBirthDate(LocalDate.of(1981, 4, 1)).withAddress(AddressFixture.TEST_ADDRESS).withRoles(roles).build();
+        UserDetailsImpl userDetails = new UserDetailsImpl(user);
         List<String> expectedRoles = USER.getRoles().stream().map(r -> r.getName().name()).collect(toList());
         Collection<GrantedAuthority> authorities = userDetails.getAuthorities();
         assertThat(authorities, notNullValue());
         assertThat(authorities.size(), equalTo(expectedRoles.size()));
-        List<String> roles = authorities.stream().map(GrantedAuthority::getAuthority).collect(toList());
-        assertThat(roles, equalTo(expectedRoles));
+        List<String> actualRoles = authorities.stream().map(GrantedAuthority::getAuthority).collect(toList());
+        assertThat(actualRoles, equalTo(Collections.singletonList(RoleNameEnum.ROLE_USER.name())));
     }
 
     @Test
